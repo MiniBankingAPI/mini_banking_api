f98d7d2ee75f20dbc63e7d7883a2157e6cabf983
student <student@itismeucci.com>
2026-03-17 12:30:57 +0100

    endpoints definition

--- mini_banking_api/php/index.php
+++ mini_banking_api/php/index.php
@@ -5,30 +5,26 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/controllers/AlunniController.php';
+require __DIR__ . '/controllers/MovimentiController.php';
+require __DIR__ . '/controllers/SaldoController.php';
+
 
 $app = AppFactory::create();
 
-//Movimenti
-$app->get('/accounts/1/transactions', "MovimentiController:list_movemets");
-$app->get('/accounts/1/transactions/5', "MovimentiController:details_movement");
-$app->post('/accounts/1/deposits', "MovimentiController:register_deposit");
-$app->post('/accounts/1/withdrawals', "MovimentiController:register_");
+// Movimenti
+$app->get('/accounts/{idAccount}/transactions', "MovimentiController:list_movements");
+$app->get('/accounts/{idAccount}/transactions/{idTransaction}', "MovimentiController:details_movement");
+$app->post('/accounts/{idAccount}/deposits', "MovimentiController:register_deposit");
+$app->post('/accounts/{idAccount}/withdrawals', "MovimentiController:register_withdrawal");
+$app->put('/accounts/{idAccount}/transactions/{idTransaction}', "MovimentiController:modify_movement_descripsion");
+$app->delete('/accounts/{idAccount}/transactions/{idTransaction}', "MovimentiController:eliminate_movement");
 
+// Saldo
+$app->get('/accounts/{idAccount}/balance', "SaldoController:get_balance");
 
+// Conversione del saldo
+$app->get('/accounts/{idAccount}/balance/convert/fiat', "SaldoController:convert_to_fiat");
+$app->get('/accounts/{idAccount}/balance/convert/crypto', "SaldoController:convert_to_crypto");
 
-
-$app->get('/test', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Test page");
-    return $response;
-});
-
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
-$app->get('/alunni', "AlunniController:index");
 
 $app->run();
