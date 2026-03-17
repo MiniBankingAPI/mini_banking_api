ad9688d0e163c579cafee94fcc01352805d17026
student <student@itismeucci.com>
2026-03-17 13:32:19 +0100

    add details_movement

--- mini_banking_api/php/controllers/MovimentiController.php
+++ mini_banking_api/php/controllers/MovimentiController.php
@@ -17,6 +17,17 @@ class MovimentiController
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
 
+  public function details_movement(Request $request, Response $response, $args){
   
+    $id = $args['idAccount'];
+    $idTrans = $args['idTransaction'];
+
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'hotpeppers', 'bank');
+    $result = $mysqli_connection->query("SELECT * FROM transactions WHERE account_id = '$id' AND id = '$idTrans'");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+    }
 
 }
