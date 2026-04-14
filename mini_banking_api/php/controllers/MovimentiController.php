e73ba7d0c50474309f7f84b5522127952a8fa2a9
student <student@itismeucci.com>
2026-04-14 11:46:29 +0200

    little changes

--- mini_banking_api/php/controllers/MovimentiController.php
+++ mini_banking_api/php/controllers/MovimentiController.php
@@ -20,7 +20,7 @@ class MovimentiController
     $stmt->execute();
     $results = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
-    if($result->num_rows === 0){
+    if($results->num_rows === 0){
 
       $response->getBody()->write(json_encode("ERRORE: nessun movimento trovato per questo account"));
       return $response->withHeader("Content-type", "application/json")->withStatus(404);
